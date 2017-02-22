223643952c2b06afac290a841ab474a442c15677
张新 <zhangxin@zhangxindemacbook-pro.local>
2017-02-22 22:36:08 +0800

    Initial Commit

---
+++ VXDIDemo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  VXDIDemo
+//
+//  Created by 张新 on 17/2/22.
+//  Copyright © 2017年 voidxin. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ VXDIDemo/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  VXDIDemo
+//
+//  Created by 张新 on 17/2/22.
+//  Copyright © 2017年 voidxin. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
